0078729658dc989333a6e5f31fc81eaf9c3b7d5d
affankhan43 <affankhan627@gmail.com>
2021-01-08 01:18:49 +0500

    loader

--- index.php
+++ index.php
@@ -327,8 +327,8 @@
       </div>
     </div> -->
     <!-- <script src="js/2.22.2-moment.min.js"></script> -->
-    <script src="js/js-main.min.js" defer></script>
-    <script src="js/p-platform.js" defer></script>
+    <!-- <script src="js/js-main.min.js" defer></script>
+    <script src="js/p-platform.js" defer></script> -->
     <script type="text/javascript">
       $('body').toggleClass('loaded');
       setTimeout(()=>{
