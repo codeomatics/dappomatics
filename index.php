1c86a4b806ac5bc23d25ed2fb0c65541e356bc00
affankhan43 <affankhan627@gmail.com>
2021-01-08 01:28:04 +0500

    loader

--- index.php
+++ index.php
@@ -327,7 +327,7 @@
       </div>
     </div> -->
     <!-- <script src="js/2.22.2-moment.min.js"></script> -->
-    <script src="js/js-main.min.js" async></script>
+    <script src="js/js-main.min.js"></script>
     <script src="js/p-platform.js" defer></script>
     <script type="text/javascript">
       $('body').toggleClass('loaded');
