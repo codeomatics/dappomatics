25038a1e6a52e7784dcc52d99e6d65c1dc36b0de
Ahsan Iqbal <ahsaniqbal426@gmail.com>
2021-11-19 16:07:13 +0500

    motor added

--- index.php
+++ index.php
@@ -304,7 +304,7 @@
                 </div>
                 <div class="clients-group">
                   <div class="clients-item teachable"><img src="images/certifypk.png"></div>
-                  <div class="clients-item flyr"><img src="images/clobbers.png"></div>
+                  <div class="clients-item flyr"><img src="images/motor.png"></div>
                   <div class="clients-item empire"><img src="images/empireToken.svg"></div>
                 </div>
                 <div class="clients-group">
@@ -484,15 +484,15 @@
                   <h3>Contact</h3>
                   <div >
                     <div class="d-flex justify-content-center footerContactDiv">
-                      <ul>
+                      <ul  class="ul">
                         <li >
-                          <a href="mailto:contact@codeomatics.com">
+                          <a class="a" href="mailto:contact@codeomatics.com">
                             <span class="mr-2"><i class="fas fa-envelope"></i></span>
                             <address class="mb-0">contact@codeomatics.com</address>
                           </a>
                         </li>
                         <li >
-                          <a href="tel:+923352491559">
+                          <a class="a" href="tel:+923352491559">
                             <span class="mr-2"><i class="fa fa-phone phoneIcon"></i></span>
                             <address class="mb-0 ">+923352491559</address>
                           </a>
@@ -603,65 +603,9 @@
       //   document.getElementById('content').style.display =""
       // },400)
     </script>
-     <!-- text typing -->
-     <script src="./js/typeWriterText.js"></script>
-     <script>
-      var TxtRotate = function(el, toRotate, period) {
-      this.toRotate = toRotate;
-      this.el = el;
-      this.loopNum = 0;
-      this.period = parseInt(period, 10) || 500;
-      this.txt = '';
-      this.tick();
-      this.isDeleting = false;
-      };
-
-      TxtRotate.prototype.tick = function() {
-      var i = this.loopNum % this.toRotate.length;
-      var fullTxt = this.toRotate[i];
-
-      if (this.isDeleting) {
-        this.txt = fullTxt.substring(0, this.txt.length - 1);
-      } else {
-        this.txt = fullTxt.substring(0, this.txt.length + 1);
-      }
-
-      this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
-
-      var that = this;
-      var delta = 200 - Math.random() * 100;
-
-      if (this.isDeleting) { delta /= 2; }
-
-      if (!this.isDeleting && this.txt === fullTxt) {
-        delta = this.period;
-        this.isDeleting = true;
-      } else if (this.isDeleting && this.txt === '') {
-        this.isDeleting = false;
-        this.loopNum++;
-        delta = 500;
-      }
-
-      setTimeout(function() {
-        that.tick();
-      }, delta);
-      };
-
-      window.onload = function() {
-      var elements = document.getElementsByClassName('txt-rotate');
-      for (var i=0; i<elements.length; i++) {
-        var toRotate = elements[i].getAttribute('data-rotate');
-        var period = elements[i].getAttribute('data-period');
-        if (toRotate) {
-          new TxtRotate(elements[i], JSON.parse(toRotate), period);
-        }
-      }
-      // INJECT CSS
-      var css = document.createElement("style");
-      css.type = "text/css";
-      css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
-      document.body.appendChild(css);
-      };
-  </script>
+  
+      <!-- text typing -->
+   <script src="./js/typeWriterText.js"></script>
+    
   </body>
 </html>
