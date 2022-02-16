e6ab28c7f152b02bf1661ff1413688509089934e
Affan Khan <affankhan627@gmail.com>
2022-02-16 19:33:26 +0500

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1,minimum-scale=1, maximum-scale=1, user-scalable=no">
-    <meta name="description" content="We are Codeomatics, A team of passionate blockchain enthusiasts and developers.">
+    <meta name="description" content="We are Dappomatics, A team of passionate blockchain enthusiasts and developers.">
     <meta http-equiv="Cache-control" content="public">
     <title>Dappomatics</title>
     <link rel="preload" as="font" href="fonts/Basel-Regular.woff" type="font/woff" crossorigin="anonymous">
